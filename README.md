69668e8c3fc53a9d3d8451bd126f088e839b16be
Chinaza <ugwuokechinazajames@gmail.com>
2023-05-08 23:24:53 +0100

    Started the Soft Ride Project - Driver Version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Soft Ride - Driver
+
+A Flutter Taxi Booking App. The Driver Version of the App
+
+## Getting Started
+
+A new flutter project
---
+++ android/app/src/main/kotlin/com/example/soft_ride_driver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.soft_ride_driver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
